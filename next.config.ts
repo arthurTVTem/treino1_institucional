3a395f41b1b2c7c6fc284d9fb4a7c2b4600ae810
arthurTvTem <arthur.goes@tvtem.com>
2025-07-17 14:12:22 -0300

    test: criação de navbar

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
